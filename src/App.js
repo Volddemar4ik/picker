d00a439a407f4fe4a2ea1137ce5e215dd2a731cf
Volddemar4ik <volddemar4ik@gmail.com>
2023-05-23 23:46:38 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import './App.css';
-import './style.css'
-import Modal from './modal';
+import Modal from './components/modal';
 
 
 function App() {
--- src/components/modal/index.js
+++ src/components/modal/index.js
@@ -1,58 +1,24 @@
 import { useState } from "react"
 import { defaultColors } from "./default_colors"
 import { ChromePicker } from "react-color"
+import { colorDistance, hexToRGB } from "../functions"
+import './style.css'
 
 export default function Modal({ active, setActive, setColor }) {
-    const [usedColor, setUsedColor] = useState('#000000')
     const [choosedColor, setChoosedColor] = useState('')
     const [selectedBlock, setSelectedBlock] = useState(null)
 
-    // custom picker
-    const [currenColor, setCurrentColor] = useState('#000')
+    const [currenColor, setCurrentColor] = useState('#000000')
     const handleOnChange = color => {
         setCurrentColor(color)
-        setUsedColor(color.hex)
-        findClosestColors(color.hex)
+        сlosestColors(color.hex)
     }
 
     const handleBlockClick = (blockId) => {
         setSelectedBlock(blockId)
     }
 
-    // standart picker
-    // const handleSetColor = event => {
-    //     const selectedColor = event.target.value
-    //     setUsedColor(selectedColor)
-    //     findClosestColors(selectedColor)
-    // }
-
-    // hex to rgb function
-    const hexToRGB = hex => {
-        const shortHexRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i
-        const longHexRegex = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i
-        const result = longHexRegex.exec(hex) || shortHexRegex.exec(hex)
-
-        return result
-            ? {
-                r: parseInt(result[1], 16),
-                g: parseInt(result[2], 16),
-                b: parseInt(result[3], 16),
-            }
-            : null
-    }
-
-    // color's vectors
-    const colorDistance = (color1, color2) => {
-        const components = color1.split(',')
-        const deltaR = parseInt(components[0]) - color2.r
-        const deltaG = parseInt(components[1]) - color2.g
-        const deltaB = parseInt(components[2]) - color2.b
-
-        return Math.sqrt(deltaR * deltaR + deltaG * deltaG + deltaB * deltaB)
-    }
-
-    // function closest colors
-    const findClosestColors = color => {
+    const сlosestColors = color => {
         const selectedColorRGB = hexToRGB(color)
 
         const closestColors = defaultColors
@@ -83,7 +49,6 @@ export default function Modal({ active, setActive, setColor }) {
     }
 
     function selectColor() {
-        console.log('choosedColor', choosedColor)
         if (choosedColor.RGB) {
             setColor(choosedColor)
             setActive(false)
@@ -94,13 +59,12 @@ export default function Modal({ active, setActive, setColor }) {
         <div className={active
             ? "custom-grout-color-block__modul-window-show"
             : "custom-grout-color-block__modul-window"}>
-            <div className=" modul-window">
+            <div className="modul-window">
                 <div className="modul-window__header">
                     <span className="icon-clear" onClick={() => setActive(false)}></span>
                 </div>
 
                 <div className="modul-window__picker">
-                    {/* <input type='color' className='modul-window__picker-window' onChange={handleSetColor} /> */}
                     <ChromePicker
                         color={currenColor}
                         onChange={handleOnChange}
